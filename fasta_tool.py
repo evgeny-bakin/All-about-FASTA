9abb6ee1b92872ce5b3b2d35fadd241a4f2d57ca
evgeny-bakin <jenyb@vu.spb.ru>
2018-10-12 12:02:34 +0300

    Some bugs are fixed

--- fasta_tool.py
+++ fasta_tool.py
@@ -3,6 +3,7 @@ import os
 import inspect
 from Bio import SeqIO
 import module_evg
+from module_evg import *
 
 def check_input_file(parser, file_name):
     full_file_name = os.path.abspath(file_name)
@@ -48,27 +49,33 @@ def make_arguments_parser():
     parser.add_argument('-p', dest = 'parameters', 
                         help='Parameters of requested function', 
                         required=False,
-                        type=str)
+                        type=str,
+                        default='0')
                         
     parser.add_argument('-o', dest = 'output_file', 
                         help='Output file name (without extension)', 
                         required=False,
                         type=str,
-                        default='')
+                        default='0')
                                                     
     return parser.parse_args()
 
 if __name__ == "__main__":
     
     args = make_arguments_parser()
+#    print(args.input_file)
     
     file_type = check_file_format(args.input_file)
     print("File type is {}.".format(file_type))
     
     all_functions = [item[0] for item in 
                             inspect.getmembers(module_evg, inspect.isfunction)]
-                            
+                 
+    print(all_functions)
     if args.function not in all_functions:
         print("Function '{}' is unavailable!".format(args.function))
+        exit()
         
-    exec('{}({},{},{})'.format(args.function, args.input_file, args.parameters, args.output_file))
+    my_command = "{}('{}',{},{})".format(args.function, args.input_file, args.parameters, args.output_file)
+    print(my_command)
+    exec(my_command)
--- module_evg.py
+++ module_evg.py
@@ -4,13 +4,13 @@ Example of module with functions
 
 import Bio
 
-def one_function(input_file_name, output_file_name, options):
+def one_function(input_file_name, options, output_file_name):
     
     pass
 
     return
 
-def another_function(input_file_name, output_file_name, options):
+def another_function(input_file_name, options, output_file_name):
     
     pass
 
