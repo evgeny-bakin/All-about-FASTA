74131b957012a6dea7cd4906bb25b7774011e3f9
Alisa1195 <1195alisa@gmail.com>
2018-10-16 01:07:40 +0300

    some corrections

--- module_alice.py
+++ module_alice.py
@@ -6,12 +6,12 @@ import statistics
 from Bio import SeqIO
 
     
-def fast_report(input_file_name, output_file_name, output_mode = "quiet" ):
-    with open(input_file_name, "r"):
-        read_length = [len(seq_record.seq) for seq_record in SeqIO.parse(input_file_name, file_type)]
+def fast_report(input_file, output_file, output_mode = "quiet"):
+    with open(input_file, "r"):
+        read_length = [len(seq_record.seq) for seq_record in SeqIO.parse(input_file, file_type)]
         count_reads = len(read_length)
         if file_type == "fasta" and count_reads == 1:
-            print("Length:", read_length, file = open(output_file_name, "w"))
+            print("Length:", read_length, file = open(output_file, "a"))
             if output_mode == "verbose":
                 print("The sequence length is {}".format(read_length[0]))
         elif file_type == "fastq":
@@ -19,7 +19,7 @@ def fast_report(input_file_name, output_file_name, output_mode = "quiet" ):
                   "Min length:", min(read_length),
                   "Max length:", max(read_length),
                   "Prevailing length:", statistics.mode(read_length), 
-                   file = open(output_file_name, "w"))
+                   file = open(output_file, "a"))
             if output_mode == "verbose":
                 print("The file contains {} reads. The read lengths range from {} to {}. The length of the most reads is {}.".format(count_reads, min(read_length), max(read_length), statistics.mode(read_length)))
         
