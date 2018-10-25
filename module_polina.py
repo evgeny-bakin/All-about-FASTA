ba016db46b7532ff4c50a33ac8dda3bcfad49f44
Polinaa19 <pollypavlova98@gmail.com>
2018-10-25 23:50:04 +0300

    add positional arguments

--- module_polina.py
+++ module_polina.py
@@ -1,14 +1,15 @@
 import Bio 
+from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 
 
-def complement_reverse_sequence(input_file, output_file):
+def complement_reverse_sequence(input_file, output_file, *options):
     
     with open(input_file, 'r'):
-        if file_type == ("fasta" or "fastq"):
-            for seq_record in SeqIO.parse(input_file, file_type):
-                sequence = str(repr(seq_record.seq))
+     #   if file_type == ("fasta" or "fastq"):
+        #for seq_record in SeqIO.parse(input_file, check_file_format(args.input_file)):
+        sequence = str(repr(input_file))
     
     complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).complement()
     reverse_complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).reverse_complement()
