b4be2cdfa8bc981c7f5015183172110adf842d36
Polinaa19 <pollypavlova98@gmail.com>
2018-10-25 22:54:27 +0300

    little corrections

--- module_polina.py
+++ module_polina.py
@@ -3,17 +3,17 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 
 
-def complement_reverse_sequence(args.input_file, args.output_file):
+def complement_reverse_sequence(input_file, output_file):
     
-    with open(args.input_file, 'r'):
+    with open(input_file, 'r'):
         if file_type == ("fasta" or "fastq"):
-            for seq_record in SeqIO.parse(args.input_file, file_type):
+            for seq_record in SeqIO.parse(input_file, file_type):
                 sequence = str(repr(seq_record.seq))
     
     complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).complement()
     reverse_complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).reverse_complement()
         
-    print('Complement sequence is: \n', complement_sequence, file = open(args.output_file, 'a'))
-    print('Reverse complement sequence is: \n', reverse_complement_sequence, file = open(args.output_file, 'a'))
+    print('Complement sequence is: \n', complement_sequence, file = open(output_file, 'a'))
+    print('Reverse complement sequence is: \n', reverse_complement_sequence, file = open(output_file, 'a'))
     
     return
