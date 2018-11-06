91f258965417d474bd7bfd551a5bb13b30d4fd77
Polinaa19 <pollypavlova98@gmail.com>
2018-11-06 14:21:58 +0300

    function delete reads shorter than X was added (not sure it's correctly)

--- module_polina.py
+++ module_polina.py
@@ -1,20 +1,53 @@
-import Bio 
+import Bio
+import pandas
+import progressbar
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+from progressbar import *
 
-
-def complement_reverse_sequence(input_file, output_file, *options):
+def complement_reverse_sequence(input_file, output_file, file_type):
     
     with open(input_file, 'r'):
-     #   if file_type == ("fasta" or "fastq"):
-        #for seq_record in SeqIO.parse(input_file, check_file_format(args.input_file)):
-        sequence = str(repr(input_file))
+        if file_type == "fasta" or "fastq":
+            for seq_record in SeqIO.parse(input_file, file_type):
+                sequence = str(repr(input_file))
     
-    complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).complement()
-    reverse_complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).reverse_complement()
+                complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).complement()
+                reverse_complement_sequence = Seq(sequence, IUPAC.unambiguous_dna).reverse_complement()
         
-    print('Complement sequence is: \n', complement_sequence, file = open(output_file, 'a'))
-    print('Reverse complement sequence is: \n', reverse_complement_sequence, file = open(output_file, 'a'))
+                print('Complement sequence is: \n', complement_sequence, file = open(output_file, 'a'))
+                print('Reverse complement sequence is: \n', reverse_complement_sequence, file = open(output_file, 'a'))
     
+    return
+
+
+def delete_reads_shorter(input_file, output_file, parameters):
+
+    with open(input_file, 'r'):
+
+        count_reads = len([len(seq_record.seq) for seq_record in SeqIO.parse(input_file, file_type)])
+
+        for i in range(count_reads + 1):
+            widgets = ['Calculate length for each read in file...', Percentage(), ' ',
+                       Bar(marker='0', left='[', right=']'),
+                       ' ', ETA(), ' ', FileTransferSpeed()]
+            pbar1 = ProgressBar(widgets=widgets, maxval=count_reads)
+            pbar1.start()
+            reads = pd.Series(seq_record.seq for seq_record in SeqIO.parse(input_file, 'fasta'))
+            length_of_reads = reads.str.len()
+            pbar1.update(i)
+        pbar1.finish()
+
+        print('Search reads shorter than {}'.format(parameters), 'and creating otput file')
+        for i in range(length_of_reads):
+            widgets = ['Search reads shorter than {}'.format(parameters), 'and creating otput file', Percentage(), ' ',
+                       Bar(marker='0', left='[', right=']'),
+                       ' ', ETA(), ' ', FileTransferSpeed()]
+            pbar2 = ProgressBar(widgets=widgets, maxval = count_reads)
+            pbar2.start()
+            if length_of_reads[i] < int(parameters):
+                print(reads[i], file = open(output_file, 'w'))
+            pbar2.update(i)
+        pbar2.finish()
     return
