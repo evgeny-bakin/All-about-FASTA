b6fcc0b8dfd6122c6b45ca82d9ea1e1d45427a5a
AlenaKizenko <43323627+AlenaKizenko@users.noreply.github.com>
2018-11-07 13:12:06 +0300

    function 'join_sequences' was added

--- module_alena.py
+++ module_alena.py
@@ -9,6 +9,7 @@ Created on Mon Oct 15 18:08:22 2018
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from Bio.SeqUtils import GC
+from Bio import SeqIO
 import csv
 
 def gc_content_analysis (input_file, output_file):
@@ -35,4 +36,26 @@ def gc_content_analysis (input_file, output_file):
                 result = csv.writer(csvFile, delimiter = ',')
                 for key, value in dict.items():
                     result.writerow([key, value])
-    
+
+#working on visualization of process now
+def join_sequences(input_file, parameters, output_file):
+    print('Reading your first file...')
+    with open(input_file, 'r'):
+        record1 = {}
+        for record in SeqIO.parse(input_file, file_type):
+            record1[record.id] = record
+    print('Reading your second file...')
+    with open(parameters, 'r'):
+        record2 = {}
+        for record in SeqIO.parse(parameters, file_type):
+            record2[record.id] = record
+    print('Joining your files, please, wait...')
+    with open(output_file, 'w'):
+        result = list(record1.values())
+        for k in record2:
+            if k not in record1:
+                result.append(record2[k])
+            else:
+                pass
+        SeqIO.write(result, output_file, file_type)
+    return
