ea06d5670057ba05fda010fa5e78b9d0b9ef21c7
Alisa1195 <1195alisa@gmail.com>
2018-11-27 00:32:47 +0300

    basic_statistics

--- module_alice.py
+++ module_alice.py
@@ -1,5 +1,5 @@
 '''
-This module contains "fast_report" function, providing some basic statistics for our FASTA/FASTQC file and "quality_score" function, performing assesment of average per base quality
+This module contains "basic_statistics" function, providing some basic statistics for our FASTA/FASTQ file and "quality_score" function, performing assesment of average per base quality
 '''
 import Bio
 import statistics
@@ -12,24 +12,31 @@ from Bio import SeqIO
 import time
 
 
-def fast_report(input_file, output_file, output_mode="quiet"):
+basic_statistics(input_file, output_mode = "quiet", output_file = "basic_statistics.csv"):def fast_report(input_file, output_file, output_mode="quiet"):
+
     with open(input_file, "r"):
         read_length = [len(seq_record.seq) for seq_record in SeqIO.parse(input_file, file_type)]
         count_reads = len(read_length)
-        if file_type == "fasta" and count_reads == 1:
-            print("Length:", read_length, file=open(output_file, "a"))
+        total_length = sum(read_length)
+        read_mean = statistics.mean(read_length)
+        read_mode = statistics.mode(read_length)
+        mode_percent = (read_length.count(read_mode) / count_reads) * 100
+
+        if count_reads == 1:
+            print("Length:", read_length, file = open(output_file, "a"))
             if output_mode == "verbose":
-                print("The sequence length is {}".format(read_length[0]))
-        elif file_type == "fastq":
-            print("Reads:", count_reads,
-                  "Min length:", min(read_length),
-                  "Max length:", max(read_length),
-                  "Prevailing length:", statistics.mode(read_length),
-                  file=open(output_file, "a"))
+                print("There is one sequence in your file. The sequence length is {}".format(read_length[0]))
+        else:
+            print("Reads:", count_reads, "\n"
+                  "Min length:", min(read_length), "\n"
+                  "Max length:", max(read_length), "\n"
+                  "Average length:", round(read_mean, 2), "\n"
+                  "Prevailing length:", read_mode, "({}%)".format(round(mode_percent, 1)), "\n"
+                  "Total length:", total_length,
+                   file = open(output_file, "a"))
             if output_mode == "verbose":
-                print(
-                    "The file contains {} reads. The read lengths range from {} to {}. The length of the most reads is {}.".format(
-                        count_reads, min(read_length), max(read_length), statistics.mode(read_length)))
+                print("The file contains {} reads. The read lengths range from {} to {}. The most frequent length is {} ({}% of all reads). The total length is {}.".format(count_reads,
+                min(read_length), max(read_length), statistics.mode(read_length), round(mode_percent, 1), total_length))
 
     return
 
