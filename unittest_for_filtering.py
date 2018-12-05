5e35778d08fe8fd917ce02555df747153e723c9b
Polinaa19 <pollypavlova98@gmail.com>
2018-12-06 02:37:58 +0300

    unittest

---
+++ unittest_for_filtering.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+import fasta_tool
+import module_polina
+from Bio import SeqIO
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+
+
+fasta_test =  os.path.join(os.path.dirname(__file__), 'fasta_test.fasta')
+fastq_test = os.path.join(os.path.dirname(__file__), 'fastq_test.fastq')
+fasta_res =  os.path.join(os.path.dirname(__file__), 'fasta_res')
+fastq_res = os.path.join(os.path.dirname(__file__), 'fastq_res')
+
+class TestFiltering(unittest.TestCase):
+
+    def test_fasta_min_length(file):
+        module_polina.min_length(fasta_test, 1665, fasta_res, "fasta")
+        counter = 0
+        for title, seq in SimpleFastaParser(open(fasta_res)):
+            counter += 1
+        self.assertEqual(counter, 6)
+'''
+    def test_fastq_min_length(file):
+        module_polina.min_length(fastq_test, 113, fastq_res, "fastq")
+        counter = 0
+        for title, seq, qual in FastqGeneralIterator(open(fastq_res)):
+            counter += 1
+        assert(counter == 8)
+'''
+
+if __name__ == '__main__':
+    unittest.main()
